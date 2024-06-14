9cfd156584b0b7d171c2d4de09f504da7c5f9a87
KartikaApDD <dharmadivyak@wsc.school.nz>
2024-06-14 12:36:32 +1200

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+def start_game():
+    print("Welcome to Text Adventure Game, player")
+    print("Welcome to the world, do you want to know why I sent you here?")
+    print("I need your help to break the issue of why people hating of a girl. She is Angela, after you solve the problem you can go out from here. You just to get some key to go. Good luck")
+    main_room()
