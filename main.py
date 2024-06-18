31f30ce189d5352e3dfe0b7778cae83098d85014
KartikaApDD <dharmadivyak@wsc.school.nz>
2024-06-18 12:11:31 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
-def start_game():
-    print("Welcome to Text Adventure Game, player")
-    print("Welcome to the world, do you want to know why I sent you here?")
-    print("I need your help to break the issue of why people hating of a girl. She is Angela, after you solve the problem you can go out from here. You just to get some key to go. Good luck")
-    main_room()
+class Game:
+    def __init__(self):
+        self.inventory = []
+        self.start_game()
+
+#---functions---
+
+# Welcome user and introduce the game
+def intro(): 
+    # ask name of user       
+    name = input("What is your name?")
+    #welcom user
+    print("Welcome to the adventure game!,",name)
+    print("Want to know what is your task that you need to solve on this game?")
+    #intro of game
+    print("You will get some of question when you starting the game and of course you need to answer it! If you are incorrect, you die!")
+    print("You need to get at least 1 apple to end the game and escape, but how to get that apple?")
+    print("I will provide story that can support you to answer the question on the game and there might be a hint on the question and hopiing you can solve it, dear player ^^")
+    print("This is the time, goodluck")
+
+#introduction bg story game
+def start_game(self):
+    print("This is what the games about, i will explain before you will need to start the real game!")
+    print("put story")
+    print("Hope you can escape, even though a bit chance to win this game ^^")
+    self.main_room()
+def main_room(self):
+    print("You need to choose your own path here! Please choose the best choices")
+    choice = input("where you want to go? (village/class/house/exit):").capitalize()
+
+    if choice == "village":
+        self.village_room()
+    elif choice == "class":
+        self.class_room()
+    elif choice == "house": 
+        self.house_room()
+    elif choice == "exit":
+        print("You can't go before start the game, sorry!")
+        self.main_room()
+    else:
+        print("greattt!")
