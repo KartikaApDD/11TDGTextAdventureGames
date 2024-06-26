f8ab6c09c640e273e16e7ff7ddca22c02d8da995
gallagec <160548604+gallagec@users.noreply.github.com>
2024-06-27 11:29:43 +1200

    quiz task

--- chanduli.py
+++ chanduli.py
@@ -1,16 +1,53 @@
-#---functions---
-class Game:
-    def __init__(self):
+
+print("welcome to the game, player")
+print("input your name first")
+#input the password
+number_OF_cHaracters= 9
+if number_OF_cHaracters==9:
+    print("password is acceptable.")
+else:
+    print("try again")
+instructions= "you have only 3  attempts to answer each question", "you can go next level by completing the current level"
+QUESTION_FORMAT="{}/nA.{}.B{}. C{}. D{}"
+score=0
+#ask the first question from user.
+question= "which element exist in liquid state in room temperature?"
+a= "sodium"
+b= "bromine"
+c="helium"
+d= "pottasium"
+amswer=  b
+if amswer==b:
+    print ("you have answered correctly.")
+    score+=5
+    print("click here to see the next question")
+else:
+    print("your answer is wrong")
+    print("you have to select another option")
+    if amswer==b:
+        print ("you have answered correctly.")
+        print("click here to see the next question")
+    elif print("you have not option. Your score is progrecing. "):     
+        ending_score= score
+input("choose the best path for your carreer")
+print("maths", "physics", "biology","chemistry")
+if "maths":
+    print ("why did you choose that?")
+    print("why did you choose that?"):
+else :print("enter again")
+
+class game:
+    def in_it(self):
         self.start_game()
-
-#---MAIN CODE---
-
-#welcom user + intro
-    def start_game(self):
-        name = input("What is your name?") 
-        print("Welcome to the adventure game! This gonna be really funny,",name)
+#---functions---
+# Welcome user and introduce the game
+    def intro(): 
+        # ask name of user       
+        name = input("What is your name?")
+        #welcom user + intro
+        print("Welcome to the adventure game!,",name)
         print("Want to know what is your task that you need to solve on this game?. You JUST GET one question when you starting the game and of course you need to answer it! If you are incorrect, you can't escape and back to your world! Get correct answer and one apple to escape, but don't worry I will give you some story line that might be can help you to answer the question. We will you give some options to go, choose the best choices! Good luck, player.")
-#introduction bg story game
+        #introduction bg story game
         print("This is what the games about, i will explain before you will need to start the real game!")
         print("There is a girl who is hated with a lot of people with no reason, her family and her friends do not even talk with her. Whenever she goes to her family village their family is not even looking or talk with her. When she goes to the class to study their friends even does not talk with her and not looking at her like they can not see her. When she goes back to home her family do not even say hello to her and their face has no expression. Why their friends and family not talk with her even just say hello to her? Can you help to find the reason?")
         print("Hope you can escape, even though a bit chance to win this game ^^")
@@ -61,39 +98,29 @@ class Game:
 #Main code
 Game ()
 
-print("welcome to the game, player")
-print("input your name first")
-#input the password
-number_OF_cHaracters= 9
-if number_OF_cHaracters==9:
-    print("password is acceptable.")
-else:
-    print("try again")
-instructions= "you have only 3  attempts to answer each question", "you can go next level by completing the current level"
-QUESTION_FORMAT="{}/nA.{}.B{}. C{}. D{}"
-score=0
-#ask the first question from user.
-question= "which element exist in liquid state in room temperature?"
-a= "sodium"
-b= "bromine"
-c="helium"
-d= "pottasium"
-amswer=  b
-if amswer==b:
-    print ("you have answered correctly.")
-    score+=5
-    print("click here to see the next question")
-else:
-    print("your answer is wrong")
-    print("you have to select another option")
-    if amswer==b:
-        print ("you have answered correctly.")
-        print("click here to see the next question")
-    elif print("you have not option. Your score is progrecing. "):     
-        ending_score= score
-input("choose the best path for your carreer")
-print("maths", "physics", "biology","chemistry")
-if "maths":
-    print ("why did you choose that?")
-    print("why did you choose that?"):
-else :print("enter again")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.............
